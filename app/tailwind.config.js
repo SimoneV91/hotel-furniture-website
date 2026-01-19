cadd90bc948afe6b48ee458904d334a03d3749c3
Simone Vanin <you@example.com>
2026-01-20 00:15:24 +0100

    Fix Tailwind purge

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{ts,tsx}",
-    "./pages/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}"
+    "./app/**/*.{ts,tsx,js,jsx}",       // tutte le pagine app router
+    "./pages/**/*.{ts,tsx,js,jsx}",     // pagine legacy
+    "./components/**/*.{ts,tsx,js,jsx}", // componenti riutilizzabili
   ],
   theme: {
     extend: {},
