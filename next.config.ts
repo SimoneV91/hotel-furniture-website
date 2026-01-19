6a27142544fb398fefd8130eb9555e8c25e975e4
Simone Vanin <you@example.com>
2026-01-20 00:02:19 +0100

    Aggiornamento sito

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "/hotel-furniture-website", // nome del repo
-  assetPrefix: "/hotel-furniture-website/", // fa puntare i CSS/JS correttamente
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        pathname: '/**',
       },
     ],
   },
