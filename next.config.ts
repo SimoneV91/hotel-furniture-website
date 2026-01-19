1d6532c16b8d0f4285ed702a80920e0a2847eca4
Simone Vanin <you@example.com>
2026-01-19 23:32:55 +0100

    Fix asset paths for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/hotel-furniture-website', // <- aggiungi questo
+  output: "export",
+  basePath: "/hotel-furniture-website", // nome del repo
+  assetPrefix: "/hotel-furniture-website/", // fa puntare i CSS/JS correttamente
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**",
       },
     ],
   },
