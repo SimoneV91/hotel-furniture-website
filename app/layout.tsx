8985324db4a75b1bd41fe640501c2588ae4e8129
Simone Vanin <you@example.com>
2026-04-21 21:54:10 +0200

    Deploy static site

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
 import RecaptchaProvider from "./components/RecaptchaProvider";
 import CookieConsentBanner from "./components/CookieConsentBanner";
 import AnalyticsScripts from "./components/analytics/AnalyticsScripts";
